f97d18df94a17608b735181a27fe3b3952b862cd
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-07-26 21:56:40 +0530

    init

---
+++ src/Components/Frame.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Frame extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <p>ipod</p> );
+    }
+}
+ 
+export default Frame;
+
+
