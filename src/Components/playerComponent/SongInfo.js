2daecff84cdaee964a662e840a045d87728d9933
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-08-19 21:38:31 +0530

    sgimg

--- src/Components/playerComponent/SongInfo.js
+++ src/Components/playerComponent/SongInfo.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../../Static/Player.css";
-import song from "../../Static/assets/song.jpg";
+// import song from "https://upload.wikimedia.org/wikipedia/en/5/57/Something_Just_Like_This.png";
 import { FaStar } from "react-icons/fa";
 
 const SongInfo = (props) => {
   return (
     <div>
       <div className="song-cover-img">
-        <img src={song} alt="" id="song-cover-image" />
+        <img src="https://upload.wikimedia.org/wikipedia/en/5/57/Something_Just_Like_This.png" alt="" id="song-cover-image" />
       </div>
       <div className="song-metadata">
         <marquee direction="left" scrollamount="4">
